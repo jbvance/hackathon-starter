b1303272f4f28beaf35a9b4d1c0f74104bc20058
Ubuntu <ubuntu@ip-172-31-17-53.us-east-2.compute.internal>
2019-11-15 18:10:14 +0000

    add gravatar to User model

--- models/User.js
+++ models/User.js
@@ -182,6 +182,18 @@ module.exports = function(db, DataTypes) {
     return bcrypt.compare(password, this.password);
   }
   
+  User.prototype.gravatar = function gravatar(size) {
+  if (!size) {
+    size = 200;
+  }
+  if (!this.email) {
+    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+  }
+  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+};
+
+  
   
   return User;
 };
